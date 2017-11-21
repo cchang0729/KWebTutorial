6e7f3153185ea48b2ede6fd6361929681cc12dbc
changyoung <cchang1005@gmail.com>
2017-11-21 18:01:37 +0900

    successfully write-able boards

--- routes/boards.js
+++ routes/boards.js
@@ -2,11 +2,29 @@ var express = require('express');
 var router = express.Router();
 var Client = require('mariasql');
 
+//show all
+router.get('/', function(req, res, next) {
+    var client = new Client({
+        host : '127.0.0.1',
+        db: 'openholo',
+        user : 'keti',
+        password : 'keti'
+    });
+    client.query("select * from boards", function(err, rows){
+        console.log(rows);
+        if(err)
+            throw err;
+        res.render('boards', {rows : rows});
+    });
+});
+
+router.get('/new', function(req, res, next) {
+    res.render('board_new');
+});
 
 /* GET board, require authentication. */
 router.get('/:id', function(req, res, next) {
     //connect to database
-
     var id = req.params.id;
     var c = new Client({
         host : 'localhost',
@@ -16,46 +34,57 @@ router.get('/:id', function(req, res, next) {
     });
 
     c.query("select * from boards where id="+id, function(err, rows){
-        console.log(rows[0].name);
         res.render('board', {rows:rows[0]});
     });
     c.end();
 });
-//
-// /* post on board ==> append to database and redirect to home*/
-// router.post('/', function(req, res, next){
-//     //load data from req
-//     var row = {
-//         username: req.body['username'],
-//         email: req.body['email'],
-//         title: req.body['title'],
-//         content: req.body['content']
-//     };
-//     //console.log(JSON.stringify(row));
-//     //connect to database
-//     mongoc.connect('mongodb://127.0.0.1:27017/KETIdb', function(err, db){
-//         if(err) {
-//             throw err;
-//         }
-//         db.collection('boards').insertOne(row);
-//         db.close();
-//     });
-//
-//     //append on database
-//     console.log("Successfuly inserted");
-//
-//     //redirect to '/', sending message
-//     res.redirect('/');
-// });
-//
-// router.delete('/:id', function(req, res, next){
-//     //check authentication, or redirect to login page
-//     if(req.user)
-//     {
-//         next();
-//     } else {
-//         res.redirect('/login');
-//     }
-// });
+
+/* GET board, require authentication. */
+router.get('/:id/:method', function(req, res, next) {
+    //connect to database
+    var id = req.params.id;
+    var is_delete = req.params.method === "delete";
+    if(is_delete){
+        var c = new Client({
+            host : 'localhost',
+            db : 'openholo',
+            user : 'keti',
+            password : 'keti'
+        });
+        console.log('delete it!');
+        //delete operation in database!1
+
+        res.redirect('/boards');
+    }
+    c.end();
+});
+
+/* post on board ==> append to database and redirect to home*/
+router.post('/', function(req, res, next){
+    //load data from req
+    var date = new Date();
+    var row = {
+        username: req.body['name'],
+        title: req.body['title'],
+        contents: req.body['contents'],
+        date: date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString() + "-" + date.getDate().toString()
+    };
+    var dbQuery = "INSERT INTO  openholo.boards (`name`, `title`, `contents`, `date`) VALUES ('"+row.username+"','"+ row.title + "','" + row.contents + "','"+ row.date + "');";    //console.log(JSON.stringify(row));
+    console.log(dbQuery);
+    //connect to database
+    var client = new Client({
+        host : '127.0.0.1',
+        db: 'openholo',
+        user : 'keti',
+        password : 'keti'
+    });
+    client.query( dbQuery, function(err, rows){
+        if(err)
+            throw err;
+        res.redirect('/boards');
+    });
+    client.end();
+});
+
 
 module.exports = router;
