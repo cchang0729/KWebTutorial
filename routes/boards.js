a745f3a8f87c27a79dd90b75040b474aa79eb75d
changyoung <cchang1005@gmail.com>
2017-11-24 18:16:33 +0900

    file upload clear

--- routes/boards.js
+++ routes/boards.js
@@ -3,14 +3,20 @@ var router = express.Router();
 var Client = require('mariasql');
 var dbconfig = require('../config/dbconfig');
 var passport = require('passport');
-var uploader = require('multer')({dest : 'public/upload/'}).array('upload-files', 10);
 var path = require('path');
+var fs = require('fs');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination : function(req, file, cb){cb(null, './public/upload/');},
+    filename : function(req, file, cb){cb(null, new Date().getTime().toString()+"-"+ file.originalname);}
+})
+var uploader = multer({storage:storage}).single('upload-files');
 
 //show all
 router.get('/', function(req, res, next) {
     var client = new Client(dbconfig);
     client.query("select * from boards", function(err, rows){
-        console.log(rows);
+        //console.log(rows);
         if(err)
             throw err;
         res.render('boards', {rows : rows});
@@ -28,7 +34,6 @@ router.get('/:id([0-9]+$)', function(req, res, next) {
     var c = new Client(dbconfig);
 
     c.query("select * from boards where id="+id, function(err, rows){
-        rows[0].filename = "file.jpg";      //filename
         res.render('board', {rows:rows[0]});
     });
     c.end();
@@ -42,6 +47,17 @@ router.get('/:id/delete', function(req, res, next){
     //connect to database
     var id = req.params.id;
     var c = new Client(dbconfig);
+    c.query("select * FROM `openholo`.`boards` WHERE `id`=" + id.toString(), function(err, rows){
+        if(rows.info.numRows > 0){      //file remove
+            var filepath = path.resolve('./public/upload/' + rows[0].filename);
+            fs.stat(filepath, function(err, stats){
+                if(err) { throw err; }
+                fs.unlink(filepath, function(err){
+                    if(err) throw err
+                });
+            });
+        }
+    });
     //delete operation in database!1
     c.query("DELETE FROM `openholo`.`boards` WHERE `id`=" + id.toString(), function(err, rows){
         if(err)
@@ -58,26 +74,28 @@ router.get('/download/:filename', function(req, res, next){
     next();
 }, function(req, res, next) {
     //connect to database
-    console.log(path.resolve('public/upload/' + req.params.filename));
     res.download(path.resolve('public/upload/' + req.params.filename));
 });
 
 /* post on board ==> append to database and redirect to home*/
 router.post('/', uploader, function(req, res, next){
-    console.log(req.files['upload-files']);
+    console.log(req.file);
     //load data from req
     var date = new Date();
     var row = {
         username: req.body['name'],
         title: req.body['title'],
         contents: req.body['contents'],
+        filename: req.file.filename,
         date: date.getFullYear().toString() + "-" + (date.getMonth() + 1).toString() + "-" + date.getDate().toString()
     };
     // console.log(dbQuery);
     //connect to database
     var client = new Client(dbconfig);
-    var prep = client.prepare("INSERT INTO `openholo`.`boards` (`name`, `title`, `contents`, `date`) VALUES (:username, :title, :contents, :date);");
+    var prep = client.prepare("INSERT INTO `openholo`.`boards` (`name`, `title`, `contents`, `filename`, `date`) " +
+        "VALUES (:username, :title, :contents, :filename, :date);");
 
+    console.log(prep(row));
     client.query( prep(row), function(err, rows){
         if(err)
             throw err;
